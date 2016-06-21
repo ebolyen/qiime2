daca88b584b130d44a16e347ac8676d709baca06
ebolyen <ebolyen@gmail.com>
2016-06-21 14:42:08 -0700

    Improve primitives and adds Metadata

--- qiime/plugin.py
+++ qiime/plugin.py
@@ -13,9 +13,10 @@ import pkg_resources
 import qiime.sdk
 import qiime.core.type.grammar as grammar
 from qiime.core.type import (SemanticType, is_semantic_type, Int, Str, Float,
-                             Visualization)
+                             Metadata, MetadataCategory, Visualization)
 
-__all__ = ['load', 'Plugin', 'Int', 'Str', 'Float', 'SemanticType']
+__all__ = ['load', 'Plugin', 'Int', 'Str', 'Float', 'Metadata',
+           'MetadataCategory', 'SemanticType']
 
 
 def load(plugin_name, plugin_entry_point_name=None):
